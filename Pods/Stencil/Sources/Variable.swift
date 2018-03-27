e92d4c9fbad499a7ed25bd02ba07c0a2a9e13a3f
David Jennes <david.jennes@gmail.com>
2018-03-27 13:15:42 +0200

    TEMPORARY: use fix from https://github.com/kylef/Stencil/pull/204

--- Pods/Stencil/Sources/Variable.swift
+++ Pods/Stencil/Sources/Variable.swift
@@ -100,16 +100,10 @@ public struct Variable : Equatable, Resolvable {
         current = object.value(forKey: bit)
 #endif
       } else if let value = current {
-        let mirror = Mirror(reflecting: value)
-        current = mirror.descendant(bit)
-
-        if current == nil {
-          return nil
-          // mirror returns non-nil value even for nil-containing properties
-          // so we have to check if its value is actually nil or not
-        } else if let current = current, String(describing: current) == "nil" {
-          return nil
-        }
+		current = Mirror(reflecting: value).getValue(for: bit)
+		if current == nil {
+			return nil
+		}
       } else {
         return nil
       }
@@ -178,4 +172,39 @@ func parseFilterComponents(token: String) -> (String, [Variable]) {
     .smartSplit(separator: ",")
     .map { Variable($0) }
   return (name, variables)
+}
+
+extension Mirror {
+	func getValue(for key: String) -> Any? {
+		let result = descendant(key)
+		if result == nil {
+			// go through inheritance chain to reach superclass properties
+			return superclassMirror?.getValue(for: key)
+		} else if let result = result {
+			guard String(describing: result) != "nil" else {
+				// mirror returns non-nil value even for nil-containing properties
+				// so we have to check if its value is actually nil or not
+				return nil
+			}
+			if let result = (result as? AnyOptional)?.wrapped {
+				return result
+			} else {
+				return result
+			}
+		}
+		return result
+	}
+}
+
+protocol AnyOptional {
+	var wrapped: Any? { get }
+}
+
+extension Optional: AnyOptional {
+	var wrapped: Any? {
+		switch self {
+		case let .some(value): return value
+		case .none: return nil
+		}
+	}
 }
